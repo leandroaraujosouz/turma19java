ee6afb4e50f915e30caa85f47f5b7370e17ead5e
leandro souza <leandro_a_souza@hotmail.com>
2021-04-05 17:15:08 -0300

    Exercicio de imposto

---
+++ java/AULAS/src/Exemplo/troca.java
@@ -0,0 +1,24 @@
+package Exemplo;
+
+import java.util.Scanner;
+
+public class troca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+		int a,b,troca;
+		System.out.println("Digite o valor de a: ");
+		a = leia.nextInt();
+		System.out.println("Digite o valor de b: ");
+		b = leia.nextInt();
+		
+		troca = a;
+		a=b;
+		b=troca;
+		
+		System.out.printf("A = %d\nB = %d\n",a,b);
+		
+	}
+
+}
